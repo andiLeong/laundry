94fbb99e49da70667c211175da593bcb8c921acf
andiLeong <alwayssmile20150724@gmail.com>
2023-07-09 19:06:57 +0800

    make spa login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -34,5 +34,7 @@ class LoginController extends Controller
 
         Auth::login($user);
         $request->session()->regenerate();
+
+        return $user;
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
             'last_name' => fake()->lastName(),
             'phone' => '09' . rand(1000,3000) . rand(30001,99999),
             'phone_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => 'password', // password
         ];
     }
 
