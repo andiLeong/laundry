900f89e2c38aa9b0a69893d7d74c2fdabecd465c
andiLeong <alwayssmile20150724@gmail.com>
2023-08-27 11:10:29 +0800

    ad slug column to product table and update product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
@@ -16,8 +17,10 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        $name = fake()->word() . rand(10000,99999);
         return [
-            'name' => fake()->word(),
+            'name' => $name,
+            'slug' => Str::slug($name),
             'price' => rand(50,300),
             'stock' => rand(100,300),
         ];
