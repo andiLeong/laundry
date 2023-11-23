9e778bf3c7149e2d912f177b5ec10947245fc464
andiLeong <alwayssmile20150724@gmail.com>
2023-11-23 13:48:35 +0800

    debug and fix 2

--- app/Http/Controllers/AdminStatController.php
+++ app/Http/Controllers/AdminStatController.php
@@ -13,25 +13,26 @@ class AdminStatController extends Controller
     {
         $query = Order::query();
 
-        if($request->get('days') !== 'all'){
+        if ($request->get('days') !== 'all') {
             $query->passDays($request->get('days'));
         }
 
-        $promotionCount = OrderPromotion::whereIn('order_id',$query->pluck('id'))->count();
+        $promotionCount = OrderPromotion::whereIn('order_id', $query->pluck('id'))->count();
         $orderCount = $query->count();
         $rate = $orderCount > 0
-            ? round($promotionCount / $orderCount,2) * 100
+            ? (int)round(($promotionCount / $orderCount) * 100)
             : 0;
+
         return [
             'user_count' => User::count(),
             'order_count' => $orderCount,
             'total_order_amount' => $query->sum('total_amount'),
             'promotion_count' => $promotionCount,
             'order_promotion_rate' => $rate,
-            'foo' => $orderCount > 0 ? round($promotionCount / $orderCount,2) : 'no',
+            'foo' => $orderCount > 0 ? round($promotionCount / $orderCount, 2) : 'no',
             'foo1' => $orderCount > 0 ? $promotionCount / $orderCount : 'no',
-            'foo2' => $orderCount > 0 ? round($promotionCount / $orderCount,2) : 'no',
-            'foo3' => $orderCount > 0 ? round($promotionCount / $orderCount,2) * 100 : 'no',
+            'foo2' => $orderCount > 0 ? round($promotionCount / $orderCount, 2) : 'no',
+            'foo3' => $orderCount > 0 ? round($promotionCount / $orderCount, 2) * 100 : 'no',
             'foo4' => 0.29 * 100,
         ];
     }
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,9 +13,23 @@ class ExampleTest extends TestCase
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
-//        $arr = [0.29*100];
-//        $arr = [0.29];
-//        dump(json_encode($arr));
+//        $promotionCount = 21;
+//        $orderCount = 73;
+//        $res = (int) round(($promotionCount/$orderCount) * 100);
+////        dump(is_int($res));
+//        dump(json_encode([$res]));
+//        dd('done');
+//
+////        dd($res);
+//        $value =  intval(0.29 * 100);
+//        dump($value);
+//        dump(0.29 * 100);
+//        dump(is_float($value));
+//        dump(is_int($value));
+//        dd($value);
+//        $arr = [$value];
+////        $arr = [0.29];
+//        dump(json_encode($arr, JSON_NUMERIC_CHECK));
 //        dump(json_encode([0.29*100]));
 //        dd(0.29*100);
 
