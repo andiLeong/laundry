f6c1a2e08738998144ba1097a499710360e19772
andiLeong <alwayssmile20150724@gmail.com>
2024-02-22 21:24:31 +0800

    add test to make sure order status must be update from previous state

--- tests/Feature/OnlineOrderStatusUpdateTest.php
+++ tests/Feature/OnlineOrderStatusUpdateTest.php
@@ -58,14 +58,46 @@ class OnlineOrderStatusUpdateTest extends TestCase
     }
 
     /** @test */
+    public function it_gets_404_if_order_id_not_exists(): void
+    {
+        $this->signInAsAdmin()
+            ->patchJson('/api/admin/online-order/999999', ['type' => 'pickup'])
+            ->assertNotFound();
+    }
+
+    /** @test */
+    public function if_online_order_is_delivered_it_cant_update_status(): void
+    {
+        $this->onlineOrder->update(['status' => OnlineOrderStatus::PICKUP->value]);
+        $message = $this->update(['type' => 'pickup'])->assertStatus(400)->json('message');
+        $this->assertEquals($message, 'Order must from pending pickup to picked up');
+    }
+
+    /** @test */
+    public function it_gets_400_if_online_order_status_is_not_pending_when_update_to_pickup(): void
+    {
+        $this->onlineOrder->update(['status' => OnlineOrderStatus::DELIVERED->value]);
+        $this->update(['type' => 'pickup'])->assertStatus(400);
+        $this->update(['type' => 'delivery'])->assertStatus(400);
+    }
+
+    /** @test */
+    public function it_gets_400_if_online_order_status_is_not_pickup_when_update_to_delivered(): void
+    {
+        $this->onlineOrder->update(['status' => OnlineOrderStatus::PENDING_PICKUP->value]);
+        $message = $this->update(['type' => 'delivery'])->assertStatus(400)->json('message');
+        $this->assertEquals($message, 'Order need to pickup first, then set delivery status');
+    }
+
+    /** @test */
     public function pending_online_order_can_be_updated_to_pickup(): void
     {
         $this->assertNull($this->onlineOrder->pickup_at);
         $this->update(['type' => 'pickup'])
             ->assertStatus(200);
 
-        $this->assertEquals(OnlineOrderStatus::PICKUP->toLower(),$this->onlineOrder->fresh()->status);
-        $this->assertEquals(now()->toDateTimeString(),$this->onlineOrder->fresh()->pickup_at);
+        $this->assertEquals(OnlineOrderStatus::PICKUP->toLower(), $this->onlineOrder->fresh()->status);
+        $this->assertEquals(now()->toDateTimeString(), $this->onlineOrder->fresh()->pickup_at);
     }
 
     /** @test */
@@ -77,12 +109,12 @@ class OnlineOrderStatusUpdateTest extends TestCase
         $this->update(['type' => 'delivery'])
             ->assertStatus(200);
 
-        $this->assertEquals(OnlineOrderStatus::DELIVERED->toLower(),$this->onlineOrder->fresh()->status);
-        $this->assertEquals(now()->toDateTimeString(),$this->onlineOrder->fresh()->deliver_at);
+        $this->assertEquals(OnlineOrderStatus::DELIVERED->toLower(), $this->onlineOrder->fresh()->status);
+        $this->assertEquals(now()->toDateTimeString(), $this->onlineOrder->fresh()->deliver_at);
     }
 
     public function update($arr = [])
     {
-        return $this->signInAsAdmin()->patchJson($this->endpoint,$arr);
+        return $this->signInAsAdmin()->patchJson($this->endpoint, $arr);
     }
 }
